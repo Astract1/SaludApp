180957fc59d1e28fd71727327e125b5599e7d6b0
Astract <diegojimenez1924@gmail.com>
2024-09-11 01:00:28 -0500

    implement Fragment and nav bar

---
+++ app/src/main/java/com/astract/saludapp/desafios.kt
@@ -0,0 +1,30 @@
+package com.astract.saludapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class desafios : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_desafios, container, false)
+    }
+
+    companion object {
+
+    }
+}
---
+++ app/src/main/java/com/astract/saludapp/habitos.kt
@@ -0,0 +1,32 @@
+package com.astract.saludapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+
+class habitos : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_habitos, container, false)
+    }
+
+    companion object {
+
+    }
+}
