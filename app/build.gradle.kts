7031953931b2f973be0490cb683858f8e6006f5b
Astract <diegojimenez2432@gmail.com>
2024-09-12 11:46:22 -0500

    Fix toolbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.navigation:navigation-fragment:2.8.0")
+    implementation("androidx.navigation:navigation-ui:2.8.0")
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
--- app/src/main/java/com/astract/saludapp/MainActivity.kt
+++ app/src/main/java/com/astract/saludapp/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.astract.saludapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setupNavegacion()
+    }
+
+    private fun setupNavegacion() {
+        try {
+            val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+            NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController)
+        } catch (e: Exception) {
+            Log.e("MainActivity", "Error configurando la navegación", e)
         }
     }
 }
