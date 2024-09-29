3222d35dfb303bfc583db73c48a70dc0bd41c106
Astract <diegojimenez1924@gmail.com>
2024-09-29 18:01:16 -0500

    Implemnt imc Meta

--- app/src/main/java/com/astract/saludapp/imc.kt
+++ app/src/main/java/com/astract/saludapp/imc.kt
@@ -2,7 +2,11 @@ package com.astract.saludapp
 
 import android.app.AlertDialog
 import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,7 +24,6 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-
 class imc : Fragment() {
     private lateinit var pesoEditText: TextInputEditText
     private lateinit var alturaEditText: TextInputEditText
@@ -29,12 +32,18 @@ class imc : Fragment() {
     private lateinit var iconArrow: ImageView
     private lateinit var rootView: View
     private lateinit var dbHelper: MyDatabaseHelper
+    private lateinit var botonGuardarMeta: MaterialButton
+    private lateinit var valorMetaTextView: TextView
+    private lateinit var valorMeta: TextInputEditText
+
+
+    private var metaIMC: Double? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflar el layout para este fragmento
         rootView = inflater.inflate(R.layout.fragment_imc, container, false)
         return rootView
     }
@@ -42,45 +51,45 @@ class imc : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Inicializar el dbHelper aquí
         dbHelper = MyDatabaseHelper(requireContext())
 
         val fechaTextView: TextView = view.findViewById(R.id.fecha2)
         val fechaActual = obtenerFechaActual()
         fechaTextView.text = fechaActual
 
-        // Inicializar las vistas
         pesoEditText = view.findViewById(R.id.peso)
         alturaEditText = view.findViewById(R.id.altura)
         valorPesoTextView = view.findViewById(R.id.valor_peso)
         calcularButton = view.findViewById(R.id.calcular_peso)
         iconArrow = view.findViewById(R.id.icon_arrow)
+        valorMetaTextView = view.findViewById(R.id.valor_meta)
+        botonGuardarMeta = view.findViewById(R.id.btnGuardarMetaIMC)
+        valorMeta = view.findViewById(R.id.inputMetaIMC)
+
+
 
         calcularButton.setOnClickListener {
             calcularIMC()
         }
 
-        iconArrow.setOnClickListener {
-            // Cargar la animación de scroll desde XML
-            val scrollAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.scroll_up)
 
-            // Aplicar la animación a la vista raíz del fragmento
+        botonGuardarMeta.setOnClickListener {
+            guardarMetaIMC()
+        }
+
+        iconArrow.setOnClickListener {
+            val scrollAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.scroll_up)
             rootView.startAnimation(scrollAnimation)
 
-            // Listener para saber cuándo termina la animación
             scrollAnimation.setAnimationListener(object : Animation.AnimationListener {
-                override fun onAnimationStart(animation: Animation?) {
-                    // Opcional: Acciones al inicio de la animación
-                }
+                override fun onAnimationStart(animation: Animation?) {}
 
                 override fun onAnimationEnd(animation: Animation?) {
                     val action = imcDirections.actionImcToHistorialImc()
-                    findNavController().navigate(action) // Navegar usando el NavController
+                    findNavController().navigate(action)
                 }
 
-                override fun onAnimationRepeat(animation: Animation?) {
-                    // No se requiere ninguna acción aquí
-                }
+                override fun onAnimationRepeat(animation: Animation?) {}
             })
         }
     }
@@ -89,59 +98,85 @@ class imc : Fragment() {
         val peso = pesoEditText.text.toString().toDoubleOrNull()
         val altura = alturaEditText.text.toString().toDoubleOrNull()
 
-
-
-        // Validar entrada de altura
         if (altura == null) {
             alturaEditText.error = "La altura no puede estar vacía"
             return
         }
-
 
         if (peso == null) {
             pesoEditText.error = "El peso no puede estar vacío"
             return
         }
 
+        val alturaMetros = altura / 100
+        val imc = peso / (alturaMetros * alturaMetros)
+        valorPesoTextView.text = String.format("IMC: %.2f", imc)
 
-        if (peso != null && altura != null) {
-            val alturaMetros = altura / 100  // Convertir altura a metros
-            val imc = peso / (alturaMetros * alturaMetros)
-
-            valorPesoTextView.text = String.format("IMC: %.2f", imc)
-
-            // Cambiar color y texto basado en el IMC
-            when {
-                imc < 18.5 -> {
-                    valorPesoTextView.setTextColor(Color.BLUE)
-                    valorPesoTextView.text = "Bajo peso\nIMC: %.2f".format(imc)
-                }
-
-                imc in 18.5..24.9 -> {
-                    valorPesoTextView.setTextColor(Color.GREEN)
-                    valorPesoTextView.text = "Normal\nIMC: %.2f".format(imc)
-                }
-
-                imc in 25.0..29.9 -> {
-                    valorPesoTextView.setTextColor(Color.YELLOW)
-                    valorPesoTextView.text = "Sobrepeso\nIMC: %.2f".format(imc)
-                }
-
-                else -> {
-                    valorPesoTextView.setTextColor(Color.RED)
-                    valorPesoTextView.text = "Obesidad\nIMC: %.2f".format(imc)
-                }
+        when {
+            imc < 18.5 -> {
+                valorPesoTextView.setTextColor(Color.BLUE)
+                valorPesoTextView.text = "Bajo peso\nIMC: %.2f".format(imc)
             }
 
-            // Diálogo de confirmación para guardar en el historial
-            mostrarDialogoConfirmacion(imc, peso, altura)
+            imc in 18.5..24.9 -> {
+                valorPesoTextView.setTextColor(Color.GREEN)
+                valorPesoTextView.text = "Normal\nIMC: %.2f".format(imc)
+            }
+
+            imc in 25.0..29.9 -> {
+                valorPesoTextView.setTextColor(Color.YELLOW)
+                valorPesoTextView.text = "Sobrepeso\nIMC: %.2f".format(imc)
+            }
+
+            else -> {
+                valorPesoTextView.setTextColor(Color.RED)
+                valorPesoTextView.text = "Obesidad\nIMC: %.2f".format(imc)
+            }
         }
+
+        mostrarDialogoConfirmacion(imc, peso, altura)
+
     }
 
     private fun mostrarDialogoConfirmacion(imc: Double, peso: Double, altura: Double) {
+        val pesoIdeal = calcularPesoIdeal(altura)
+        val diferencia = peso - pesoIdeal
+
+        val mensaje = when {
+            diferencia > 0 -> "Necesitas bajar ${"%.2f".format(diferencia)} kg para alcanzar tu peso ideal de ${
+                "%.2f".format(
+                    pesoIdeal
+                )
+            } kg."
+
+            diferencia < 0 -> "Estás por debajo de tu peso ideal de ${"%.2f".format(pesoIdeal)} kg."
+            else -> "¡Estás en tu peso ideal!"
+        }
+
+        val categoriaIMC = when {
+            imc < 18.5 -> "Bajo peso"
+            imc in 18.5..24.9 -> "Normal"
+            imc in 25.0..29.9 -> "Sobrepeso"
+            else -> "Obesidad"
+        }
+
+        val imcTexto =
+            "¿Deseas guardar este IMC de ${"%.2f".format(imc)} (IMC Ideal: ${"%.2f".format(22.0)}) con Peso: $peso kg y Altura: $altura cm en tu historial?\n\nCategoría: $categoriaIMC\n$mensaje"
+
+        // Crear un SpannableString para resaltar el IMC Ideal
+        val spannableString = SpannableString(imcTexto)
+        val start = imcTexto.indexOf("IMC Ideal:") + "IMC Ideal: ".length
+        val end = start + "22.00".length
+        spannableString.setSpan(
+            StyleSpan(Typeface.BOLD),
+            start,
+            end,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
         val builder = AlertDialog.Builder(requireContext())
         builder.setTitle("Guardar en Historial")
-        builder.setMessage("¿Deseas guardar este IMC de ${"%.2f".format(imc)} con Peso: $peso kg y Altura: $altura cm en tu historial?")
+        builder.setMessage(spannableString)
         builder.setPositiveButton("Sí") { dialog, _ ->
             guardarEnHistorial(imc, peso, altura)
             dialog.dismiss()
@@ -152,14 +187,16 @@ class imc : Fragment() {
         builder.show()
     }
 
-    private fun guardarEnHistorial(imc: Double, peso: Double, altura: Double) {
-        // Obtener la fecha actual
-        val fechaActual = obtenerFechaActual()
+    private fun calcularPesoIdeal(altura: Double): Double {
+        val alturaMetros = altura / 100 // Convertir altura a metros
+        val imcIdeal = 22.0 // IMC ideal
+        return imcIdeal * (alturaMetros * alturaMetros) // Peso ideal
+    }
 
-        // Llamar a la función insertHistorialIMC
+    private fun guardarEnHistorial(imc: Double, peso: Double, altura: Double) {
+        val fechaActual = obtenerFechaActual()
         dbHelper.insertHistorialIMC(imc, fechaActual, peso, altura)
 
-        // Mostrar un Toast como confirmación
         Toast.makeText(
             requireContext(),
             "IMC guardado en historial: IMC: ${"%.2f".format(imc)}, Peso: $peso kg, Altura: $altura cm",
@@ -174,5 +211,23 @@ class imc : Fragment() {
         val fechaFormateada = formatoFecha.format(fecha)
 
         return fechaFormateada.replaceFirstChar { it.titlecase(locale) }
+    }
+
+
+    private fun guardarMetaIMC() {
+        val metaIMCInput = valorMeta.text.toString().toDoubleOrNull()
+
+        if (metaIMCInput == null) {
+            valorMeta.error = "La meta no puede estar vacía"
+            return
+        }
+
+        metaIMC = metaIMCInput
+
+        valorMetaTextView.text = String.format("Meta IMC: %.2f", metaIMC)
+
+        Toast.makeText(requireContext(), "Meta de IMC guardada : $metaIMC", Toast.LENGTH_LONG)
+            .show()
+
     }
 }
